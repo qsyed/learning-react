fa8a650fa54b45e77714e567a9a5640bceda135a
Abdul Syed <dev.abdul01@gmail.com>
2021-01-23 19:07:13 -0500

    learning how to call a js fuction via embedding

--- section2-jsx/embedding_js.js
+++ section2-jsx/embedding_js.js
@@ -1,10 +1,17 @@
 // learning how to embedd js code
 
+
+function getMood(){
+    const moods = ["happy", "angry", "sad", "hungry"]
+    
+    // getting a random element from the array
+    return moods[Math.floor(Math.random() * moods.length)]
+}
 class JSXDemo extends React.Component {
     render(){
         return(
             <div>
-                <h1>hi my number is {2 * 8.3}</h1>
+                <h1>Hi im feeling: {getMood()} </h1>
             </div>
             
         );
