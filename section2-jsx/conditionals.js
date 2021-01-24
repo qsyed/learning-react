06d520a542c463215bf2a0577b7b9ff5bfdaab56
Abdul Syed <dev.abdul01@gmail.com>
2021-01-23 19:45:58 -0500

    adding js function for random number upto 10

--- section2-jsx/conditionals.js
+++ section2-jsx/conditionals.js
@@ -1,7 +1,15 @@
+function getNumber(){
+    return Math.floor(Math.random() * 10) + 1;
+
+}
+
+
 class NumPicker extends React.Component {
     render(){
-        
+        return (
+            <h1>your number is ... {getNumber()} </h1>
 
+        );
     }
 }
 
