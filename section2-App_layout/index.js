2aff5865d7597c4bab607907a0e44ead38083bcf
Abdul Syed <dev.abdul01@gmail.com>
2021-01-23 20:22:03 -0500

    learning the correct way to break down an app

---
+++ section2-App_layout/index.js
@@ -0,0 +1,11 @@
+class APP extends React.Component {
+    render() {
+        return(
+        <div>
+            <Hello />
+        </div>
+        )
+    }
+  }
+
+ReactDOM.render(<APP />, document.getElementById('root'));
