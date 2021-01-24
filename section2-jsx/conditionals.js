c4379048ed6687069ca3b6a43749910a1a6a8245
Abdul Syed <dev.abdul01@gmail.com>
2021-01-23 19:54:21 -0500

    adding logic

--- section2-jsx/conditionals.js
+++ section2-jsx/conditionals.js
@@ -6,8 +6,17 @@ function getNumber(){
 
 class NumPicker extends React.Component {
     render(){
+        const num = getNumber()
         return (
-            <h1>your number is ... {getNumber()} </h1>
+            
+            <div>
+            <h1>your number is ... {num} </h1>
+            <p>{num === 7 ? 'Congrats' : "Unlucky"}</p>
+            {
+                num === 7 && 
+                <img src = "https://media3.giphy.com/media/5oGIdt1xapQ76/giphy.gif?cid=ecf05e47i3iw0ckuk6129t8d730huq4kj9zv5abswlqdlff0&rid=giphy.gif" />
+            }
+            </div>
 
         );
     }
