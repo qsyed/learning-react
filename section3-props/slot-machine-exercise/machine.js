4f3fda0f6c7be5b199342c47314478d46b961120
Abdul Syed <dev.abdul01@gmail.com>
2021-02-16 16:38:18 -0500

    deconstructing the props

--- section3-props/slot-machine-exercise/machine.js
+++ section3-props/slot-machine-exercise/machine.js
@@ -1,8 +1,9 @@
 class Machine extends React.Component {
     render(){
+        const{s1,s2,s3} = this.props;
         return (
             <div>
-                <p>{this.props.s1} {this.props.s2} {this.props.s3}</p>
+                <p>{s1} {s2} {s3}</p>
                 <p>winner/loser</p>
             </div>
         )
