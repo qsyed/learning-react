e6ba098d2495c632d10ec5c977e8af5adbd9ba7b
Abdul Syed <dev.abdul01@gmail.com>
2021-02-16 16:43:15 -0500

    adding more compoments

--- section3-props/slot-machine-exercise/index.js
+++ section3-props/slot-machine-exercise/index.js
@@ -8,6 +8,22 @@ class App extends React.Component{
                 s2 = "x"
                 s3 = "x"
                 />
+
+
+
+               <Machine
+                s1 = "x"
+                s2 = "y"
+                s3 = "x"
+                />
+
+
+                <Machine
+                s1 = "y"
+                s2 = "z"
+                s3 = "x"
+                />
+
             
             </div>
         )
