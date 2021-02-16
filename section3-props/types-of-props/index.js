def18bb5905b1b5b6b82efd93113ef7a99f05340
Abdul Syed <dev.abdul01@gmail.com>
2021-02-16 15:56:02 -0500

    adding more props

--- section3-props/types-of-props/index.js
+++ section3-props/types-of-props/index.js
@@ -2,7 +2,11 @@ class App extends React.Component{
     render(){
         return(
             <div>
-            <Hello to="Ringo" from="paul"/>
+            <Hello to="Ringo"
+             from="paul"
+             num = {3}
+             data = {[1,2,3,4,5]}
+            />
             <Hello to="Cher" from="sonny"/>
             </div>
         )
