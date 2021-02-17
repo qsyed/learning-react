6779cbc2f3a2bfc1d5318526450e7648c61ea7a8
Abdul Syed <dev.abdul01@gmail.com>
2021-02-17 16:16:31 -0500

    adding index.js template

---
+++ section3-props/looping-in-jsx/Friends.js
@@ -0,0 +1,11 @@
+class APP extends React.Component {
+    render() {
+        return(
+        <div>
+            
+        </div>
+        )
+    }
+  }
+
+ReactDOM.render(<APP />, document.getElementById('root'));
