bd638f56903aad049a3e3bf954242bd9417e1877
Abdul Syed <dev.abdul01@gmail.com>
2021-02-17 18:02:20 -0500

    adding code to remove items

--- section4-intro-create-react-app/excercise2/fruits_exerccise2/src/helpers.js
+++ section4-intro-create-react-app/excercise2/fruits_exerccise2/src/helpers.js
@@ -6,6 +6,11 @@ function choice(items){
 }
 
 function remove(items, item){
+    for(let i =0; i < items.length; i++){
+        if(items[i] === item){
+            return [ ...items.slice(0, i), ...items.slice(i+1)];
+        }
+    }
 
 }
 
