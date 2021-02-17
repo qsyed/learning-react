d1e70f06560691c540823342efd89fae4f9a7756
Abdul Syed <dev.abdul01@gmail.com>
2021-02-17 16:43:22 -0500

    adding className to machine.js

--- section3-props/styling-react/Machine.js
+++ section3-props/styling-react/Machine.js
@@ -3,7 +3,7 @@ class Machine extends React.Component {
         const{s1,s2,s3} = this.props;
         const winner = (s1 === s2) && (s2 === s3);
         return (
-            <div>
+            <div className="Machine">
                 <p>{s1} {s2} {s3}</p>
                 <p>{winner ? 'Winner!' : 'Loser!'} </p>
             </div>
