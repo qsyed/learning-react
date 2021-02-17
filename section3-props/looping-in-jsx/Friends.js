c7543e1f62208456d9d3a2d353bfca76c928916a
Abdul Syed <dev.abdul01@gmail.com>
2021-02-17 16:25:23 -0500

    adding map function to print out elements of array

--- section3-props/looping-in-jsx/Friends.js
+++ section3-props/looping-in-jsx/Friends.js
@@ -2,7 +2,13 @@ class Friend extends React.Component{
     render(){
         const {name, hobbies} = this.props;
         return(
-            <p>I am a Friend</p>
+            <div>
+                <h1>{name}</h1>
+                <ul>
+                    {hobbies.map(h => <li>{h}</li>)}
+                </ul>
+            </div>
+            
         )
     }
 }
