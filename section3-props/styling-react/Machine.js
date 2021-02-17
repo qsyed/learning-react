f241c87fd2b1334c07ad411221752f4fc38a0523
Abdul Syed <dev.abdul01@gmail.com>
2021-02-17 16:46:56 -0500

    learing about inline css

--- section3-props/styling-react/Machine.js
+++ section3-props/styling-react/Machine.js
@@ -4,7 +4,7 @@ class Machine extends React.Component {
         const winner = (s1 === s2) && (s2 === s3);
         return (
             <div className="Machine">
-                <p>{s1} {s2} {s3}</p>
+                <p style = {{fontSize: "20px"}}>{s1} {s2} {s3}</p>
                 <p>{winner ? 'Winner!' : 'Loser!'} </p>
             </div>
         )
