9632e49e97ce2df65509ac61da64bf64e3134be8
Abdul Syed <dev.abdul01@gmail.com>
2021-02-17 16:51:59 -0500

    adding logic o js to choose css colors based off of results

--- section3-props/styling-react/Machine.js
+++ section3-props/styling-react/Machine.js
@@ -5,7 +5,7 @@ class Machine extends React.Component {
         return (
             <div className="Machine">
                 <p style = {{fontSize: "20px"}}>{s1} {s2} {s3}</p>
-                <p>{winner ? 'Winner!' : 'Loser!'} </p>
+                <p className = {winner ? 'win' : 'lose'}>{winner ? 'Winner!' : 'Loser!'} </p>
             </div>
         )
     }
