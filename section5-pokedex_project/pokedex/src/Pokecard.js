8f1f5b7b873c6d4f82456ef716f56e6f75a10095
Abdul Syed <dev.abdul01@gmail.com>
2021-02-24 17:03:42 -0500

    getting an img

--- section5-pokedex_project/pokedex/src/Pokecard.js
+++ section5-pokedex_project/pokedex/src/Pokecard.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+const Poke_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 class Pokecard extends Component{
     render(){
+        let imgSrc = `${Poke_API}${this.props.id}.png`
         return (
             <div className="Pokecard">
                 <h1> {this.props.name} </h1>
+                <img src={imgSrc}/>
 
             </div>
 
