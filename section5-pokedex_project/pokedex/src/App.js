bf8729049bdb2f303452f11bda1fa574cd00d685
Abdul Syed <dev.abdul01@gmail.com>
2021-02-24 16:14:33 -0500

    rendering pokecard component

--- section5-pokedex_project/pokedex/src/App.js
+++ section5-pokedex_project/pokedex/src/App.js
@@ -1,3 +1,15 @@
 import React, {Component} from 'react';
 import Pokecard from "./Pokecard"
-import './App.css'
+import './App.css';
+
+class App extends Component{
+    render(){
+        (
+        return <div className="App">
+            <Pokecard />
+
+        </div>
+        );
+    }
+}
+
