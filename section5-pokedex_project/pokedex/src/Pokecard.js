804eb11afb9324c24796009cfc00257cc85700a5
Abdul Syed <dev.abdul01@gmail.com>
2021-03-04 16:09:05 -0500

    updating the image api

--- section5-pokedex_project/pokedex/src/Pokecard.js
+++ section5-pokedex_project/pokedex/src/Pokecard.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './Pokecard.css';
 
-const Poke_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// const Poke_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+const Poke_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let pad_numbers = (number) => (number <= 999 ? `00${number}`.slice(-3): number);
+
 
 class Pokecard extends Component{
     render(){
-        let imgSrc = `${Poke_API}${this.props.id}.png`
+        let imgSrc = `${Poke_API}${pad_numbers(this.props.id)}.png`
         return (
             <div className="Pokecard">
                 <h1 className="Pokecard-title"> {this.props.name} </h1>
