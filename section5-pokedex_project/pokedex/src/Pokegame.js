59c74721e136672bb2a16a99681da81fc5799415
Abdul Syed <dev.abdul01@gmail.com>
2021-03-04 16:14:34 -0500

    templating pokegame.js

---
+++ section5-pokedex_project/pokedex/src/Pokegame.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Pokegame extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Poke game !</h1>
+            </div>
+
+        );
+    }
+}   
+
+
+
+export default Pokegame;
+
