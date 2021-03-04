b7a94c565747995ad718a474ba39897820d1c4c6
Abdul Syed <dev.abdul01@gmail.com>
2021-03-04 15:38:27 -0500

    adding css styling to component

--- section5-pokedex_project/pokedex/src/pokedex.js
+++ section5-pokedex_project/pokedex/src/pokedex.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import Pokecard from "./Pokecard"
-
+import './Pokedex.css'
 class Pokedex extends Component{
     static defaultProps = {
         pokemon : [
@@ -21,9 +21,14 @@ class Pokedex extends Component{
 
             <div className="Pokedex">
                 <h1>Pokedex !</h1>
+
+                <div className="Pokedex-cards">
                 {this.props.pokemon.map((p) => (
                     <Pokecard id = {p.id} name = {p.name} type = {p.type} exp={p.base_experience} />
                 ))}
+
+                </div>
+                
 
             </div>
         );
